e1fd1dd760fdbcacb09bb8bf9d98f4f2a90faa19
João Guilherme <joaometropoles1415@gmail.com>
2020-04-21 18:59:42 -0300

    Of deploy

---
+++ build/precache-manifest.5a45c570f3e1a1e1b2b0225fe2f417c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e32be19bb129123dcf83a18f02c8117",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a91b1229a7ac56828f9f",
+    "url": "/static/js/2.101ab624.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.101ab624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13bc588f53b8904553b5",
+    "url": "/static/js/main.3590240e.chunk.js"
+  },
+  {
+    "revision": "1c517bc3a64630164821",
+    "url": "/static/js/runtime-main.c7487dbe.js"
+  },
+  {
+    "revision": "956c4a3d332443a8877260e7a980521f",
+    "url": "/static/media/wallpaper.956c4a3d.jpg"
+  }
+]);
